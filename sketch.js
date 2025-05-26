def5c6058fcc74b1fed467682345731ecc372c4c
Maria-deMaria <maria.medeiro.silva@escola.pr.gov.br>
2025-05-26 08:26:36 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+      function setup() {
+  createCanvas(1000, 1000);
+}
+
+function draw() {
+  ativajogo
+  
+  
+  
+  
+  
+  background(220);
+  textSize(40);
+ textSize(🐂,1,300);
+  
+  
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function ativaJogo(){
+  if (focused==true){
+   background("#0EBB79");
+   }
+  else{
+  background("#29910B");
+          }
